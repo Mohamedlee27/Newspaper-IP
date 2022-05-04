f133cdd97c991a42e03d82624d6b301297b2f05d
Mohamedlee27 <leonk8070@gmail.com>
2022-05-04 10:55:44 +0300

    added new route

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from ..requests import get_news
+from ..requests import get_news,get_source
 
 # Views
 @app.route('/')
@@ -11,3 +11,12 @@ def index():
     '''
     news = get_news()
     return render_template('index.html', news =news)
+
+@app.route('/source')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    source = get_source()
+    return render_template('source.html', source =source)
